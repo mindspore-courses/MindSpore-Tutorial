e42b1bca2fd9da1f5d01388453814687619e266a
revocat <isaiah2032@foxmail.com>
2023-05-30 22:44:30 +0800

    完成卷积神经网络，深度残差网络，循环神经网络

---
+++ tutorials/02-intermediate/convolutional_neural_network/main.py
@@ -0,0 +1,109 @@
+import gzip
+import os
+import shutil
+import urllib.request
+import zipfile
+
+import numpy as np
+
+import mindspore
+import mindspore.nn as nn
+import mindspore.dataset.vision
+import mindspore.dataset.vision.transforms as transforms
+from mindspore import ops
+import mindspore.common.dtype as mstype
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+
+file_path = '../../data/MNIST/'
+
+if not os.path.exists(file_path):
+    # 下载数据集
+    os.mkdir(file_path)
+    base_url = 'http://yann.lecun.com/exdb/mnist/'
+    file_names = ['train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz',
+                  't10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz']
+    for file_name in file_names:
+        url = (base_url + file_name).format(**locals())
+        print("正在从" + url + "下载MNIST数据集...")
+        urllib.request.urlretrieve(url, os.path.join(file_path, file_name))
+        with gzip.open(os.path.join(file_path, file_name), 'rb') as f_in:
+            with open(os.path.join(file_path, file_name)[:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        os.remove(os.path.join(file_path, file_name))
+
+image_transforms = transforms.ToTensor()
+label_transforms = transforms.ToTensor(output_type=np.int32)
+
+train_dataset = mindspore.dataset.MnistDataset(
+    dataset_dir=file_path,
+    usage='train',
+).map(operations=image_transforms, input_columns="image").batch(batch_size)
+
+test_dataset = mindspore.dataset.MnistDataset(
+    dataset_dir=file_path,
+    usage='test',
+).map(operations=image_transforms, input_columns="image").batch(batch_size=batch_size)
+
+
+class CNN(nn.Cell):
+    def __init__(self, num_classes=10):
+        super(CNN, self).__init__()
+        self.layer1 = nn.SequentialCell(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2, pad_mode='pad', has_bias=True),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.SequentialCell(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2, pad_mode='pad', has_bias=True),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Dense(7 * 7 * 32, num_classes)
+
+    def construct(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = ops.reshape(out, (ops.shape(out)[0], -1))
+        out = self.fc(out)
+        return out
+
+
+model = CNN(num_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = nn.optim.Adam(model.trainable_params(), learning_rate=learning_rate)
+train_model = nn.WithLossCell(model, loss_fn=criterion)
+train_model = nn.TrainOneStepCell(train_model, optimizer)
+
+for epoch in range(num_epochs):
+    for i, (image, label) in enumerate(train_dataset.create_tuple_iterator()):
+        total_step = train_dataset.get_dataset_size()
+        train_model.set_train()
+        label = mindspore.Tensor(label, mstype.int32)
+        loss = train_model(image, label)
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.asnumpy().item()))
+
+model.set_train(False)
+
+# Test the model
+correct = 0
+total = 0
+for image, label in test_dataset.create_tuple_iterator():
+    label = mindspore.Tensor(label, mstype.int32)
+    outputs = model(image)
+    _, predicted = ops.max(outputs.value(), 1)
+    total += label.shape[0]
+    correct += (predicted == label).sum().asnumpy().item()
+
+print('Test Accuracy of the model on the 10000 test images: {:.2f} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+save_path = './cnn.ckpt'
+mindspore.save_checkpoint(model, save_path)
